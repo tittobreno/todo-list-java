fa522af934c6a7b3e232fef99df205415f4db70c
Breno <brenomoraes97@yahoo.com>
2023-10-11 00:28:27 -0300

    feat: created controller

--- todolist/src/main/java/br/com/breno/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/breno/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.breno.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
 
-        System.out.println(userModel.name);
+    public UserModel create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
 
+        if (user != null) {
+            System.out.println("Usuário já existe!");
+            return null;
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
